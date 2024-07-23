648835fc0f7b6ddfee82ad56e0bc2dc8f0552931
Gabriel Jordan <gabrijordan3@gmail.com>
2024-07-22 23:14:59 -0300

    Alterar estilos da página inicial e utilizar provider

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+import { useTranslations } from "next-intl";
 import styles from "./page.module.css";
+import Filter from "@/components/shared/Filter/Filter";
+
+import { FilterProvider } from "@/contexts/FilterContext";
+import Results from "@/components/ProjectsPage/results/Results";
 
 export default function Projects() {
+    const t = useTranslations("ProjectsPage");
+
     return (
-        <main>
-            <h1>Projects page</h1>
-        </main>
+        <FilterProvider>
+            <main className={`container ${styles.main}`}>
+                <Filter />
+                <Results />
+            </main>
+        </FilterProvider>
     );
 }
