6d21714472b3622d52079d123897a3d1cbf00378
Gabriel Jordan <gabrijordan3@gmail.com>
2024-08-28 21:20:50 -0300

    teste hotjar

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -21,29 +21,19 @@ export default function HomePage() {
     const t = useTranslations("HomePage");
 
     useEffect(() => {
-        if (
-            typeof window !== "undefined" &&
-            process.env.NODE_ENV === "production"
-        ) {
-            (function (h, o, t, j, a, r) {
-                h.hj =
-                    h.hj ||
-                    function () {
-                        (h.hj.q = h.hj.q || []).push(arguments);
-                    };
-                h._hjSettings = { hjid: 5114057, hjsv: 6 };
-                a = o.getElementsByTagName("head")[0];
-                r = o.createElement("script");
-                r.async = 1;
-                r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(
-                window,
-                document,
-                "https://static.hotjar.com/c/hotjar-",
-                ".js?sv=",
-            );
-        }
+        (function (h, o, t, j, a, r) {
+            h.hj =
+                h.hj ||
+                function () {
+                    (h.hj.q = h.hj.q || []).push(arguments);
+                };
+            h._hjSettings = { hjid: 5114057, hjsv: 6 };
+            a = o.getElementsByTagName("head")[0];
+            r = o.createElement("script");
+            r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
     }, []);
 
     return (
